e6e47af190518899783c0db6f540d2f36b59f296
code1005 <corina.cernolev@mail.ru>
2023-05-29 18:18:20 +0300

    Java collections, arrays, etc

--- src/Lab4/EntityManagement.java
+++ src/Lab4/EntityManagement.java
@@ -114,7 +114,6 @@ public class EntityManagement {
 
             String attribute = "name";
             displayRecordDetails(entities, attribute);
-
             // Updating data
             Entity entityToUpdate = entities.get(1);
             entityToUpdate.updateData("age", "30");
